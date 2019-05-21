b3b3746a5ed24bb407d9e1b59258d98eb78b0ef2
eleo <ehuayhuap@unsa.edu.pe>
2019-05-20 21:35:26 -0500

    Resolucion de los capitulos 03 al capitulo 05.

---
+++ VRM_Cap03-05/Assets/ButtonExecute.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+public class ButtonExecute : MonoBehaviour
+{
+    private GameObject currentButton;
+
+    void Update()
+    {
+        Transform camera = Camera.main.transform;
+        Ray ray = new Ray(camera.position, camera.rotation *
+        Vector3.forward);
+        RaycastHit hit;
+        GameObject hitButton = null;
+        PointerEventData data = new PointerEventData
+        (EventSystem.current);
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform.gameObject.tag == "Button")
+            {
+                hitButton = hit.transform.parent.gameObject;
+            }
+        }
+        if (currentButton != hitButton)
+        {
+            if (currentButton != null)
+            { // unhighlight
+                ExecuteEvents.Execute<IPointerExitHandler>(currentButton,
+                data, ExecuteEvents.pointerExitHandler);
+            }
+            currentButton = hitButton;
+            if (currentButton != null)
+            { // highlight
+                ExecuteEvents.Execute<IPointerEnterHandler>
+                (currentButton, data,
+                ExecuteEvents.pointerEnterHandler);
+            }
+        }
+    }
+}
